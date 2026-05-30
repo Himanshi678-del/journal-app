39f04f577fb81644d0283a3ed044c33b54e646a4
himanshi <khuranahimanshi100@gmail.com>
2026-05-30 14:47:23 +0530

    Implement JWT authentication and authorization

--- src/main/java/net/engineeringdigest/journalApp/Controller/PublicController.java
+++ src/main/java/net/engineeringdigest/journalApp/Controller/PublicController.java
@@ -1,16 +1,34 @@
 package net.engineeringdigest.journalApp.Controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.Entity.User;
 import net.engineeringdigest.journalApp.Service.UserService;
+import net.engineeringdigest.journalApp.Service.UserServiceDetailsImpl;
+import net.engineeringdigest.journalApp.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public")
+@Slf4j
 public class PublicController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserServiceDetailsImpl userServiceDetails;
+
+    @Autowired
+    private JwtUtils jwtUtil;
 
     @GetMapping("/health-check")
     public String healthCheck(){
@@ -18,8 +36,23 @@ public class PublicController {
     }
 
 
-    @PostMapping("/create-user")
+    @PostMapping("/signup")
     public void createUser(@RequestBody User user){
         userService.saveNewUser(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user){
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+            UserDetails userDetails = userServiceDetails.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        }catch (Exception e){
+            log.error("exception occurred during authentication ",e);
+            return new ResponseEntity<>("Incorrect username & pwd",HttpStatus.BAD_REQUEST);
+        }
+
+
     }
 }
